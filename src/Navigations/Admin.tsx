7ac24f162fbfd0b84d0ca75ac443deb47a462903
Decagon <decagon@Alexander.local>
2022-06-02 22:29:02 +0100

    Updated Project Folder

--- src/Navigations/Admin.tsx
+++ src/Navigations/Admin.tsx
@@ -3,32 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import Dashboard from '../pages/Dashboard/Dashboard'
 import Login from '../pages/Login/Login'
 import Register from '../pages/Register/Register'
-
-import Overview from "../pages/AdminOverview/AdminOverview";
-import Blog from "../pages/Blog/Blog";
-import Faq from "../pages/Faq/Faq";
-import Pages from "../pages/AdminPages/Pages";
-import { CaseStudy } from "../pages/caseStudy/CaseStudy";
-import { CreateBlog } from "../pages/createBlog/CreateBlog";
+import ForgetPassword from '../pages/ForgetPassword/ForgetPassword'
+import ResetPassword from '../pages/ResetPassword/ResetPassword'
 
 const Admin = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />}> 
-      
-      <Route index element={<Overview />} />
-            <Route path="pages" element={<Pages />} />
-            <Route path="blog" element={<Blog />} />
-            <Route path="faqs" element={<Faq />} />
-            <Route path="accountsettings" element={<Faq />} />
-            <Route path="casestudy" element={<CaseStudy />} />
-            <Route path="createblog" element={<CreateBlog />} />
-      
-      
-      
-      </Route>
+      <Route path="/dashboard*" element={<Dashboard />} />
+      <Route path="/forget-password" element={<ForgetPassword />} />
+      <Route path="/reset-password/:userId/:token" element={<ResetPassword />} />
       
     </Routes>
   )
