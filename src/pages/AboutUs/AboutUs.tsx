860258094d8750c993bb2e71b97c56553ad79ab1
Decagon <decagon@Alexander.local>
2022-06-06 09:40:11 +0100

    Refactor About Page

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -35,7 +35,7 @@ const AboutUs = () => {
             In publishing and graphic design, Lorem ipsum is a placeholder text 
             commonly used to demonstrate the visual form of a document or a typeface without relying 
             on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. 
-            It is also used to temporarily replace text in a process called greeking, which allows 
+            It is also used to temporarily replace text in a process called greeking, which allows.
             </p>
             <p className={Styles.para2}>
             <strong>2nd Paragraph</strong> designers to consider the form of a webpage or publication, 
