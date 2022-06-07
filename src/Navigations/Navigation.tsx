9e8aef0c47c07b83cc36325e34c9bbb1fd2bc9df
rxgoodness <adedokuntoyyib@gmail.com>
2022-06-07 13:36:00 +0100

    jobDetails and adminViewBlogs

--- src/Navigations/Navigation.tsx
+++ src/Navigations/Navigation.tsx
@@ -4,12 +4,15 @@ import Clients from "./Clients";
 import AdminLogin from "../pages/AdminLogin/AdminLogin"
 import Pagespage from "../pages/Pagespage/Pages-page";
 
+
 const Navigation = () => {
   return (
     <BrowserRouter>
       <Routes>
           <Route path="/*" element={<Clients />} />
           <Route path="/admin*" element={<Admin />} />
+          
+
       </Routes>
     </BrowserRouter>
     
